0075b13544ea6a9522b112789513159484193943
IsuruX98 <isurusanka98@gmail.com>
2024-04-24 10:54:58 +0530

    hero created

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,24 @@
 import React from "react";
 import { useAuth } from "../context/authContext";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../components/NavBar/Navbar";
+import Hero from "../components/Hero/Hero";
+import Footer from "../components/Footer/Footer";
 
 const Home = () => {
   const { user, isLoggedIn } = useAuth();
   const navigate = useNavigate();
 
-  if (!isLoggedIn) {
-    // If user is already logged in, redirect to home page
-    navigate("/login");
-  }
+  // if (!isLoggedIn) {
+  //   // If user is already logged in, redirect to home page
+  //   navigate("/login");
+  // }
 
   return (
     <div>
-      <h2>Welcome to the Home Page</h2>
-      {user && (
-        <div>
-          <p>Name: {user.name}</p>
-          <p>Email: {user.email}</p>
-          {/* Add other user details here */}
-        </div>
-      )}
+      <Navbar />
+      <Hero />
+      <Footer />
     </div>
   );
 };
